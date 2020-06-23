1934162087a9ae7f6f9c61cb449c668c14f474ba
Kha <doduckha9d@gmail.com>
2020-06-23 18:13:32 +0700

    commit update new function

---
+++ RamdomNameGame/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RamdomNameGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
